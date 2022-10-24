1349fe1272fded41668b5958630804be54026075
Rishi <git@fushinori.me>
2022-10-24 15:28:22 +0530

    Add advice generator app

---
+++ advice-generator-app-main/src/components/AdviceCard.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import Styles from "./AdviceCard.module.css";
+
+const { adviceCard } = Styles;
+
+interface Advice {
+  id: number;
+  advice: string;
+}
+
+const defaultAdvice: Advice = {
+  id: 69,
+  advice: "Loading...",
+};
+
+const errorAdvice: Advice = {
+  id: 404,
+  advice: "Failed to load advice. Please try again.",
+};
+
+export default function AdviceCard() {
+  const [advice, setAdvice] = useState<Advice>(defaultAdvice);
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function startFetching() {
+      const advice = await fetchAdvice();
+      if (!ignore) {
+        setAdvice(advice);
+      }
+    }
+
+    startFetching();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  const handleClick = async () => {
+    setAdvice(defaultAdvice);
+    const newAdvice = await fetchAdvice();
+    setAdvice(newAdvice);
+  };
+
+  const fetchAdvice = async (): Promise<Advice> => {
+    const response = await fetch("https://api.adviceslip.com/advice", {
+      cache: "no-cache",
+    }).then((response) => response);
+    if (!response.ok) {
+      return errorAdvice;
+    }
+    const adviceSlip = await response.json();
+    return adviceSlip.slip as Advice;
+  };
+
+  return (
+    <div className={adviceCard}>
+      <h1>Advice #{advice.id}</h1>
+      <p>"{advice.advice}"</p>
+      <picture>
+        <source
+          srcSet="/pattern-divider-desktop.svg"
+          media="(min-width: 40em)"
+        />
+        <img src="/pattern-divider-mobile.svg" aria-hidden="true" alt="" />
+      </picture>
+      <button onClick={handleClick}>
+        <img src="/icon-dice.svg" alt="" />
+      </button>
+    </div>
+  );
+}
