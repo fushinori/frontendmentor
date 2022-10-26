573bf06750f93aa6b0513ef91d37597f7b80980b
Rishi <git@fushinori.me>
2022-10-26 16:43:33 +0530

    [Tailwind config] Extend colours instead, add custom font size

--- news-homepage-main/tailwind.config.cjs
+++ news-homepage-main/tailwind.config.cjs
@@ -2,17 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      "soft-orange": "hsl(35, 77%, 62%)",
-      "soft-red": "hsl(5, 85%, 63%)",
-      "off-white": "hsl(36, 100%, 99%)",
-      "grayish-blue": {
-        400: "hsl(233, 8%, 79%)",
-        700: "hsl(236, 13%, 42%)",
+    extend: {
+      fontSize: {
+        sm: "1.5rm",
       },
-      "dark-blue": "hsl(240, 100%, 5%)",
+      colors: {
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": {
+          400: "hsl(233, 8%, 79%)",
+          700: "hsl(236, 13%, 42%)",
+        },
+        "dark-blue": "hsl(240, 100%, 5%)",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
