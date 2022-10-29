17427500f082b0bba3c95016e80354f32dfe6d6d
Rishi <git@fushinori.me>
2022-10-29 21:38:46 +0530

    Add overlay when opening nav menu on mobile

--- news-homepage-main/src/components/Header/NavMobile.tsx
+++ news-homepage-main/src/components/Header/NavMobile.tsx
@@ -8,23 +8,29 @@ interface Props {
 export default function NavMobile({ links }: Props) {
   const $isMenuOpen = useStore(isMenuOpen);
 
-  let className =
+  let navClassName =
     "fixed top-0 right-0 md:hidden bg-off-white w-2/3 h-screen pl-6";
+  let overlayClassName =
+    "fixed top-0 left-0 w-1/3 h-screen md:hidden bg-black opacity-50";
   if (!$isMenuOpen) {
-    className += " hidden";
+    navClassName += " hidden";
+    overlayClassName += " hidden";
   }
 
   return (
-    <nav id="primary-nav" className={className}>
-      <ul className="relative top-40 flex flex-col gap-5 text-lg">
-        {links.map((link) => (
-          <li key={link}>
-            <a className="hover:text-soft-red" href="/">
-              {link}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <nav id="primary-nav" className={navClassName}>
+        <ul className="relative top-40 flex flex-col gap-5 text-lg">
+          {links.map((link) => (
+            <li key={link}>
+              <a className="hover:text-soft-red" href="/">
+                {link}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className={overlayClassName}></div>
+    </>
   );
 }
