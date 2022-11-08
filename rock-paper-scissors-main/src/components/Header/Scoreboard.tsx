c2834cb4dc3bc0f4982511e1b9f267deaf5cd19c
Rishi <git@fushinori.me>
2022-11-08 15:08:09 +0530

    Add Header component

---
+++ rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
@@ -0,0 +1,14 @@
+import { score } from "../scoreStore";
+import { useStore } from "@nanostores/preact";
+
+export default function Scoreboard() {
+  const $score = useStore(score);
+  return (
+    <div class="bg-white flex flex-col items-center rounded w-1/4 p-2">
+      <h2 class="text-blue-100 font-semibold uppercase text-xs tracking-widest">
+        Score
+      </h2>
+      <p class="text-slate text-4xl font-bold">{$score}</p>
+    </div>
+  );
+}
