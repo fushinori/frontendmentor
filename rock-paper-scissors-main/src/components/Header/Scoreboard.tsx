97ad2ee7383def856c8e4e3f9f37ae8660794fbe
Rishi <git@fushinori.me>
2022-11-09 18:09:51 +0530

    Use updated colours in header

--- rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
+++ rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
@@ -4,11 +4,11 @@ import { useStore } from "@nanostores/preact";
 export default function Scoreboard() {
   const $score = useStore(score);
   return (
-    <div class="bg-white flex flex-col items-center rounded w-1/4 p-2">
+    <div class="bg-white-100 flex flex-col items-center rounded w-1/4 p-2">
       <h2 class="text-blue-100 font-semibold uppercase text-xs tracking-widest">
         Score
       </h2>
-      <p class="text-slate text-4xl font-bold">{$score}</p>
+      <p class="text-slate-100 text-4xl font-bold">{$score}</p>
     </div>
   );
 }
