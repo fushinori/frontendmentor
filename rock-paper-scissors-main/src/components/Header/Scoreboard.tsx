8479f372ea9baacb0f86c91854dd0d89c4e95818
Rishi <git@fushinori.me>
2022-12-05 18:53:35 +0530

    Minor stylistic changes to Header

--- rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
+++ rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
@@ -5,10 +5,10 @@ export default function Scoreboard() {
   const $score = useStore(score);
   return (
     <div class="bg-white-100 flex flex-col items-center rounded w-1/4 p-2">
-      <h2 class="text-blue-100 font-semibold uppercase text-xs tracking-widest">
+      <h2 class="text-blue-100 font-semibold uppercase text-[0.6rem] tracking-widest">
         Score
       </h2>
-      <p class="text-slate-100 text-4xl font-bold">{$score}</p>
+      <p class="text-indigo-400 text-4xl font-bold">{$score}</p>
     </div>
   );
 }
