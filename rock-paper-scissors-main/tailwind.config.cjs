17804aacba5200b607e7b1c9a8470d4b83a2067c
Rishi <git@fushinori.me>
2022-12-05 18:54:35 +0530

    Modify box shadow, add black colour

--- rock-paper-scissors-main/tailwind.config.cjs
+++ rock-paper-scissors-main/tailwind.config.cjs
@@ -7,17 +7,14 @@ module.exports = {
         100: "hsl(229, 64%, 46%)",
         400: "hsl(230, 89%, 62%)",
         500: "hsl(230, 89%, 65%)",
-        900: "hsl(230, 66%, 46%)",
       },
       red: {
         400: "hsl(349, 70%, 56%)",
         500: "hsl(349, 71%, 52%)",
-        900: "hsl(347, 76%, 35%)",
       },
       yellow: {
         400: "hsl(39, 89%, 49%)",
         500: "hsl(40, 84%, 53%)",
-        900: "hsl(29,77%, 43%)",
       },
       indigo: {
         100: "hsl(214, 47%, 23%)",
@@ -33,14 +30,15 @@ module.exports = {
         300: "hsl(0, 0%, 85%)",
         500: "hsl(215, 100%, 94%)",
       },
+      black: "hsl(0, 0%, 0%)",
     },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
       boxShadow: {
-        "inner-top": "inset 0 3px var(--tw-shadow-color)",
-        "inner-bottom": "inset 0 -5px var(--tw-shadow-color)",
+        "inner-top": "inset 0 5px rgb(0 0 0 / 0.1)",
+        "outer-disk": "inset 0 -5px rgb(0 0 0 / 0.2), 0 4px rgb(0 0 0 / 0.2)",
       },
     },
   },
