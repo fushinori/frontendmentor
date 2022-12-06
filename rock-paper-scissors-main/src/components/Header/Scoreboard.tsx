19eb270b65c6a2a0cc6e830fd8aaf0d919af86ee
Rishi <git@fushinori.me>
2022-12-06 22:25:48 +0530

    Add desktop styles to Header

--- rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
+++ rock-paper-scissors-main/src/components/Header/Scoreboard.tsx
@@ -4,11 +4,11 @@ import { useStore } from "@nanostores/preact";
 export default function Scoreboard() {
   const $score = useStore(score);
   return (
-    <div class="bg-white-100 flex flex-col items-center rounded w-1/4 p-2">
-      <h2 class="text-blue-100 font-semibold uppercase text-[0.6rem] tracking-widest">
+    <div class="bg-white-100 flex flex-col items-center rounded md:rounded-lg w-1/4 md:w-[22%] py-2 md:py-3">
+      <h2 class="text-blue-100 font-semibold uppercase text-[0.6rem] md:text-lg tracking-widest">
         Score
       </h2>
-      <p class="text-indigo-400 text-4xl font-bold">{$score}</p>
+      <p class="text-indigo-400 text-4xl font-bold md:text-6xl">{$score}</p>
     </div>
   );
 }
