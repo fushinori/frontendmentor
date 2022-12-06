6e156d21be2783f87f72deaae476c147623f462d
Rishi <git@fushinori.me>
2022-12-06 22:56:31 +0530

    Add desktop styles for GameResult component

--- rock-paper-scissors-main/src/components/Game/GameResult.tsx
+++ rock-paper-scissors-main/src/components/Game/GameResult.tsx
@@ -26,26 +26,40 @@ export default function GameResult() {
   }, []);
 
   return (
-    <section class="flex flex-col items-center w-full gap-20">
-      <h2 class="sr-only">Main Game Section</h2>
-      <div class="flex justify-between w-full">
+    <>
+      {/* Mobile result section */}
+      <section class="flex flex-col items-center w-full gap-20 md:hidden">
+        <h2 class="sr-only">Main Game Section</h2>
+        <div class="flex justify-between gap-10">
+          <ChoiceComponent choice={userChoice.get()} text="You Picked" />
+          {computerDidChoose ? (
+            <ChoiceComponent choice={$computerChoice} text="The House Picked" />
+          ) : (
+            <ChoiceComponent choice={null} text="The House Picked" />
+          )}
+        </div>
+        {matchOver && <ResultScreen result={matchResult.get()} />}
+      </section>
+      {/* Desktop result section */}
+      <section class="hidden md:flex items-center gap-20">
+        <h2 class="sr-only">Main Game Section</h2>
         <ChoiceComponent choice={userChoice.get()} text="You Picked" />
+        {matchOver && <ResultScreen result={matchResult.get()} />}
         {computerDidChoose ? (
           <ChoiceComponent choice={$computerChoice} text="The House Picked" />
         ) : (
           <ChoiceComponent choice={null} text="The House Picked" />
         )}
-      </div>
-      {matchOver && <ResultScreen result={matchResult.get()} />}
-    </section>
+      </section>
+    </>
   );
 }
 
 const PlaceHolder = () => {
   return (
     // Outer div same size as Game Disk so no displacement
-    <div class="w-32 h-32 rounded-full grid place-items-center">
-      <div className="bg-black opacity-10 w-28 h-28 rounded-full" />
+    <div class="w-32 h-32 md:w-72 md:h-72 rounded-full grid place-items-center">
+      <div className="bg-black opacity-10 w-28 h-28 md:w-56 md:h-56 rounded-full" />
     </div>
   );
 };
@@ -57,9 +71,20 @@ interface ChoiceProps {
 
 const ChoiceComponent = ({ choice, text }: ChoiceProps) => {
   return (
-    <div class="flex flex-col items-center justify-center gap-6">
-      {choice ? <Disk choice={choice} /> : <PlaceHolder />}
-      <p class="uppercase tracking-widest text-white-100 font-bold">{text}</p>
-    </div>
+    <>
+      {/* Mobile */}
+      <div class="flex flex-col items-center justify-center gap-6 md:hidden">
+        {choice ? <Disk choice={choice} size="large" /> : <PlaceHolder />}
+        <p class="uppercase tracking-widest text-white-100 font-bold">{text}</p>
+      </div>
+
+      {/* Desktop */}
+      <div class="hidden md:flex flex-col items-center justify-center gap-16">
+        <p class="uppercase tracking-widest text-white-100 font-bold text-2xl">
+          {text}
+        </p>
+        {choice ? <Disk choice={choice} size="large" /> : <PlaceHolder />}
+      </div>
+    </>
   );
 };
