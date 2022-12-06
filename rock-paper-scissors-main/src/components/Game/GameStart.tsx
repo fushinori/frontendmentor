33f155901b3edd26b089f04736e0bccc34894334
Rishi <git@fushinori.me>
2022-12-06 22:54:59 +0530

    Add desktop styles for GameStart component

--- rock-paper-scissors-main/src/components/Game/GameStart.tsx
+++ rock-paper-scissors-main/src/components/Game/GameStart.tsx
@@ -1,18 +1,25 @@
 import Disk from "./Disk";
 import { Choice } from "./gameStore";
 
-const rockStyles = "absolute bottom-0";
-const scissorsStyles = "absolute top-[-50px] right-0";
-const paperStyles = "absolute top-[-50px] left-0";
+const rockStyles = "absolute bottom-0 md:bottom-[-50px]";
+const scissorsStyles =
+  "absolute top-[-50px] right-0 md:right-[7rem] md:top-[-100px]";
+const paperStyles =
+  "absolute top-[-50px] left-0 md:top-[-100px] md:left-[7rem]";
 
 export default function GameStart() {
   return (
-    <section class="relative grid place-items-center px-8 top-10">
+    <section class="relative grid place-items-center px-8 top-10 w-full">
       <h2 class="sr-only">Main Game Section</h2>
-      <img class="w-full" src="/bg-triangle.svg" alt="" aria-hidden="true" />
-      <Disk choice={Choice.Paper} extraStyles={paperStyles} />
-      <Disk choice={Choice.Scissors} extraStyles={scissorsStyles} />
-      <Disk choice={Choice.Rock} extraStyles={rockStyles} />
+      <img
+        class="w-full md:w-1/2"
+        src="/bg-triangle.svg"
+        alt=""
+        aria-hidden="true"
+      />
+      <Disk choice={Choice.Paper} extraStyles={paperStyles} size="big" />
+      <Disk choice={Choice.Scissors} extraStyles={scissorsStyles} size="big" />
+      <Disk choice={Choice.Rock} extraStyles={rockStyles} size="big" />
     </section>
   );
 }
