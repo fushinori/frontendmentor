fe80a23f3f97dee1ccc498807d191b5106a7666c
Rishi <git@fushinori.me>
2022-12-07 15:41:29 +0530

    Add desktop styles for rules modal

--- rock-paper-scissors-main/src/components/Rules/Modal.tsx
+++ rock-paper-scissors-main/src/components/Rules/Modal.tsx
@@ -3,15 +3,25 @@ import { useEffect, useRef } from "preact/hooks";
 import { isModalOpen } from "./modalStore";
 
 export default function Modal() {
-  const ref = useRef<HTMLButtonElement>(null);
+  const mobileRef = useRef<HTMLButtonElement>(null);
+  const desktopRef = useRef<HTMLButtonElement>(null);
   const $isModalOpen = useStore(isModalOpen);
 
   const changeModalState = () => {
     isModalOpen.set(!$isModalOpen);
   };
 
+  const focusOnButton = () => {
+    const width = window.innerWidth;
+    if (width >= 768) {
+      desktopRef.current?.focus;
+    } else {
+      mobileRef.current?.focus;
+    }
+  };
+
   useEffect(() => {
-    ref.current?.focus();
+    focusOnButton();
     document.addEventListener("keydown", handleKeyDown);
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
@@ -22,27 +32,65 @@ export default function Modal() {
     changeModalState();
   };
 
+  const noPropagate = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
   const handleKeyDown = (e: KeyboardEvent) => {
     if (["Esc", "Escape"].includes(e.key)) {
       changeModalState();
     }
     // Only one focusable element so keep focus on it
     if (e.key === "Tab") {
-      ref.current?.focus();
+      e.preventDefault();
+      focusOnButton();
     }
   };
 
   return (
-    <div
-      className="absolute h-full w-full bg-white-100 top-0 flex flex-col items-center justify-center gap-28 z-10"
-      role="dialog"
-      onKeyDown={handleKeyDown}
-    >
-      <h2 className="uppercase text-3xl font-bold text-indigo-100">Rules</h2>
-      <img src="/image-rules.svg" alt="" aria-hidden="true" />
-      <button ref={ref} onClick={handleClick}>
-        <img src="/icon-close.svg" alt="Close Rules" />
-      </button>
-    </div>
+    <>
+      {/* Mobile */}
+      <div
+        className="md:hidden absolute top-0 h-full w-full bg-white-100 flex flex-col items-center justify-center gap-28 z-10"
+        role="dialog"
+        onKeyDown={handleKeyDown}
+      >
+        <h2 className="uppercase text-3xl font-bold text-indigo-100">Rules</h2>
+        <img src="/image-rules.svg" alt="" aria-hidden="true" />
+        <p class="sr-only">
+          The outcome of the game is determined by 3 simple rules: Rock wins
+          against scissors. Scissors win against paper. Paper wins against rock.
+        </p>
+        <button ref={mobileRef} onClick={handleClick}>
+          <img src="/icon-close.svg" alt="Close Rules" />
+        </button>
+      </div>
+      {/* Desktop */}
+      <div
+        class="hidden md:grid absolute top-0 h-full w-full bg-black bg-opacity-60 place-items-center"
+        // Close modal when user clicks outside
+        onClick={handleClick}
+        onKeyDown={handleKeyDown}
+      >
+        <div
+          class="flex flex-col items-center bg-white-100 py-8 rounded-md px-8 gap-12"
+          // Don't close modal when user clicks inside
+          onClick={noPropagate}
+        >
+          <div class="flex justify-between items-center w-full">
+            <h2 class="uppercase text-4xl font-bold text-indigo-100">Rules</h2>
+            <button ref={desktopRef} onClick={handleClick}>
+              <img src="/icon-close.svg" alt="Close Rules" />
+            </button>
+          </div>
+          <img src="/image-rules.svg" alt="" aria-hidden="true" class="px-8" />
+          <p class="sr-only">
+            The outcome of the game is determined by 3 simple rules: Rock wins
+            against scissors. Scissors win against paper. Paper wins against
+            rock.
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
