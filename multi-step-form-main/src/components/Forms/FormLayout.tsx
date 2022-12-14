770235fbe85bb19cc0603056dd58233e057290de
Rishi <git@fushinori.me>
2022-12-14 21:21:27 +0530

    Add common form layout

---
+++ multi-step-form-main/src/components/Forms/FormLayout.tsx
@@ -0,0 +1,29 @@
+import { useStore } from "@nanostores/preact";
+import type { ComponentChildren } from "preact";
+import { submitted } from "../store";
+import type { FormInfo } from "./Form";
+
+interface Props {
+  info: FormInfo;
+  children: ComponentChildren;
+}
+
+export default function FormLayout({ info, children }: Props) {
+  const $submitted = useStore(submitted);
+  return (
+    <section
+      class="absolute flex flex-col gap-2 bg-white mx-4 px-6 py-8 rounded-lg top-24"
+      aria-labelledby="form-title"
+    >
+      {!$submitted && (
+        <>
+          <h2 id="form-title" class="text-2xl font-bold text-marine-blue">
+            {info.title}
+          </h2>
+          <p class="text-cool-gray">{info.description}</p>
+        </>
+      )}
+      {children}
+    </section>
+  );
+}
