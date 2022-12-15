536fb1f042d25ab5b2b06757bd479a0430333566
Rishi <git@fushinori.me>
2022-12-15 17:23:10 +0530

    Add button component

---
+++ multi-step-form-main/src/components/Buttons/Button.tsx
@@ -0,0 +1,35 @@
+interface Props {
+  type: "next" | "previous" | "confirm";
+  form?: string;
+  handleClick?: () => void;
+}
+
+export default function Button({ type, form, handleClick }: Props) {
+  let text;
+  let styles = "text-sm font-medium py-2 px-4 rounded-[0.2rem]";
+
+  switch (type) {
+    case "next":
+      text = "Next Step";
+      styles += " bg-marine-blue text-white";
+      break;
+
+    case "previous":
+      text = "Go Back";
+      styles += " text-cool-gray";
+      break;
+
+    case "confirm":
+      text = "Confirm";
+      styles += " bg-purplish-blue";
+      break;
+
+    default:
+      break;
+  }
+  return (
+    <button form={form} class={styles} onClick={handleClick}>
+      {text}
+    </button>
+  );
+}
