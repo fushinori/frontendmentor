668734cb8ec9863f5e035c3ab830c2744fcd0f8e
Rishi <git@fushinori.me>
2022-12-15 17:19:11 +0530

    Add map store for storing personal info state

--- multi-step-form-main/src/components/store.ts
+++ multi-step-form-main/src/components/store.ts
@@ -1,11 +1,19 @@
-import { atom } from "nanostores";
+import { atom, map } from "nanostores";
 
 export enum ActiveStep {
   Info = 1,
   Plan,
   AddOns,
   Summary,
+  Done,
+}
+
+export interface InfoFormData {
+  name: string;
+  email: string;
+  phone: string;
 }
 
 export const activeStep = atom(ActiveStep.Info);
 export const submitted = atom(false);
+export const info = map<InfoFormData>({ name: "", email: "", phone: "" });
