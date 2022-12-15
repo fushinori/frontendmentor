b3305e0283989c3b094a6b94d62e4883458fdc91
Rishi <git@fushinori.me>
2022-12-15 17:23:38 +0530

    Add the strip that houses the two buttons

---
+++ multi-step-form-main/src/components/Buttons/Strip.tsx
@@ -0,0 +1,42 @@
+import { useStore } from "@nanostores/preact";
+import { ActiveStep, activeStep } from "../store";
+import Button from "./Button";
+
+export default function Strip() {
+  const $activeStep = useStore(activeStep);
+
+  let flexDirection;
+  if ($activeStep === ActiveStep.Info) {
+    // Align next step button to the right if go back button isn't present
+    flexDirection = "flex-row-reverse";
+  } else {
+    flexDirection = "flex-row";
+  }
+
+  let form;
+  switch ($activeStep) {
+    case ActiveStep.Info:
+      form = "personal-info";
+      break;
+
+    default:
+      break;
+  }
+
+  const handlePrevious = () => {
+    activeStep.set($activeStep - 1);
+  };
+
+  return (
+    <div class={`flex ${flexDirection} justify-between w-full bg-white p-4`}>
+      {$activeStep > ActiveStep.Info && (
+        <Button type="previous" handleClick={handlePrevious} />
+      )}
+      {$activeStep < ActiveStep.Summary ? (
+        <Button type="next" form={form} />
+      ) : (
+        <Button type="confirm" />
+      )}
+    </div>
+  );
+}
