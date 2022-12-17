ff09262c4407bfd84c8ac7b84622c00d8a3c9772
Rishi <git@fushinori.me>
2022-12-17 18:35:53 +0530

    Add shadow and adjust margin in form layout

--- multi-step-form-main/src/components/Forms/FormLayout.tsx
+++ multi-step-form-main/src/components/Forms/FormLayout.tsx
@@ -12,7 +12,7 @@ export default function FormLayout({ info, children }: Props) {
   const $submitted = useStore(submitted);
   return (
     <section
-      class="absolute flex flex-col gap-2 bg-white mx-4 px-6 py-8 rounded-lg top-24"
+      class="absolute flex flex-col gap-2 bg-white mx-4 px-6 py-8 rounded-lg top-24 shadow-xl shadow-[rgba(0,0,0,0.05)]"
       aria-labelledby="form-title"
     >
       {!$submitted && (
@@ -20,7 +20,7 @@ export default function FormLayout({ info, children }: Props) {
           <h2 id="form-title" class="text-2xl font-bold text-marine-blue">
             {info.title}
           </h2>
-          <p class="text-cool-gray">{info.description}</p>
+          <p class="text-cool-gray mb-3">{info.description}</p>
         </>
       )}
       {children}
