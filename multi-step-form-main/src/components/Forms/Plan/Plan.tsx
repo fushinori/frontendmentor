465f57ffa640255e6d656fdeb24a7ec0650f830a
Rishi <git@fushinori.me>
2022-12-19 14:12:32 +0530

    Add toggle to Plan component
    Also make some minor spacing adjustments

--- multi-step-form-main/src/components/Forms/Plan/Plan.tsx
+++ multi-step-form-main/src/components/Forms/Plan/Plan.tsx
@@ -1,12 +1,16 @@
 import { Plan as PlanEnum } from "../../../types";
 import Card from "./Card";
+import Toggle from "./Toggle";
 
 export default function Plan() {
   return (
     <>
-      <Card cardPlan={PlanEnum.Arcade} />
-      <Card cardPlan={PlanEnum.Advanced} />
-      <Card cardPlan={PlanEnum.Pro} />
+      <div class="flex flex-col gap-3 mb-5">
+        <Card cardPlan={PlanEnum.Arcade} />
+        <Card cardPlan={PlanEnum.Advanced} />
+        <Card cardPlan={PlanEnum.Pro} />
+      </div>
+      <Toggle />
     </>
   );
 }
