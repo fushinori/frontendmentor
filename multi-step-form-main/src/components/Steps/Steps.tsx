cacc29869d98d25118bf70d508bc5fd24bba15c1
Rishi <git@fushinori.me>
2022-12-22 13:50:39 +0530

    Add desktop styles for the steps sidebar

--- multi-step-form-main/src/components/Steps/Steps.tsx
+++ multi-step-form-main/src/components/Steps/Steps.tsx
@@ -1,13 +1,31 @@
 import StepNumber from "./StepNumber";
 
 const steps = [1, 2, 3, 4];
+const stepNames = ["Your Info", "Select Plan", "Add-ons", "Summary"];
 
 export default function Steps() {
   return (
-    <aside class="bg-no-repeat bg-cover w-full bg-sidebar-mobile lg:bg-sidebar-desktop flex gap-4 justify-center pt-8 pb-28 h-max">
-      {steps.map((i) => (
-        <StepNumber key={i} stepNumber={i} />
-      ))}
-    </aside>
+    <>
+      {/* Mobile */}
+      <aside class="bg-no-repeat bg-cover w-full bg-sidebar-mobile flex gap-4 justify-center pt-8 pb-28 h-max lg:hidden">
+        {steps.map((i) => (
+          <StepNumber key={i} stepNumber={i} />
+        ))}
+      </aside>
+      {/* Desktop */}
+      <aside class="hidden lg:block bg-no-repeat bg-sidebar-desktop p-8 rounded-lg">
+        {steps.map((i) => (
+          <div key={i} class="flex items-center gap-4 mb-6">
+            <StepNumber stepNumber={i} />
+            <div class="uppercase">
+              <p class="text-xs text-light-gray">Step {i}</p>
+              <p class="text-alabaster text-sm font-bold tracking-widest">
+                {stepNames[i - 1]}
+              </p>
+            </div>
+          </div>
+        ))}
+      </aside>
+    </>
   );
 }
