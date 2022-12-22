ed3ace788a23b158c90a14486f51a38b7f7786ca
Rishi <git@fushinori.me>
2022-12-22 13:49:26 +0530

    Make initial changes for desktop version
    Check the screen width and arrange content accordingly
    Use grid for desktop version

--- multi-step-form-main/src/components/Forms/Form.tsx
+++ multi-step-form-main/src/components/Forms/Form.tsx
@@ -3,12 +3,13 @@ import Steps from "../Steps/Steps";
 import type { JSX } from "preact";
 import { activeStep } from "../store";
 import FormLayout from "./FormLayout";
-import Info from "./Info";
+import Info from "./Info/Info";
 import Strip from "../Buttons/Strip";
 import Plan from "./Plan/Plan";
 import { ActiveStep } from "../../types";
 import AddOns from "./AddOns/AddOns";
 import Summary from "./Summary/Summary";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 export interface FormInfo {
   title: string;
@@ -58,17 +59,57 @@ const returnForm = (step: ActiveStep) => {
 export default function Form() {
   const $activeStep = useStore(activeStep);
   const form = returnForm($activeStep);
+  const firstUpdate = useRef(true);
+
+  // Pre-rendered, so can't use window.innerWidth here
+  const [width, setWidth] = useState(0);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    // https://stackoverflow.com/a/53254028
+    // Manually update width only on the first render
+    // Event handler takes care of the rest on other renders
+    if (firstUpdate.current) {
+      handleResize();
+      firstUpdate.current = false;
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  });
+
+  let mainContent;
+  if (width >= 1024) {
+    mainContent = (
+      <div>
+        <FormLayout info={form.info}>{form.component}</FormLayout>
+        {$activeStep !== ActiveStep.Done && <Strip />}
+      </div>
+    );
+  } else {
+    mainContent = (
+      <>
+        <FormLayout info={form.info}>{form.component}</FormLayout>
+        {$activeStep !== ActiveStep.Done && <Strip />}
+      </>
+    );
+  }
+
   return (
     <main
-      class="relative flex flex-col justify-between items-center min-h-screen"
+      class="relative flex flex-col justify-between items-center min-h-screen
+      lg:static lg:grid lg:grid-cols-3"
       aria-labelledby="main-title"
     >
       <h1 id="main-title" class="sr-only">
         Multi Step Form
       </h1>
       <Steps />
-      <FormLayout info={form.info}>{form.component}</FormLayout>
-      {$activeStep !== ActiveStep.Done && <Strip />}
+      {mainContent}
     </main>
   );
 }
