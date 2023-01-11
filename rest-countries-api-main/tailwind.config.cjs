d805456cc2f338340c4f77c58f1036aefc52f3df
Rishi <git@fushinori.me>
2023-01-11 16:02:45 +0530

    Add colours

--- rest-countries-api-main/tailwind.config.cjs
+++ rest-countries-api-main/tailwind.config.cjs
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
+    colors: {
+      "dark-blue": {
+        100: "hsl(200, 15%, 8%)",
+        500: "hsl(209, 23%, 22%)",
+        700: "hsl(207, 26%, 17%)",
+      },
+      gray: {
+        100: "hsl(0, 0%, 98%)",
+        700: "hsl(0, 0%, 52%)",
+      },
+      white: "hsl(0, 0%, 100%)",
+    },
     extend: {},
   },
   plugins: [],
