9e534bf4682544dde51bf5341aa629ebc35bb70f
Rishi <git@fushinori.me>
2023-01-11 20:08:02 +0530

    Add header

---
+++ rest-countries-api-main/src/lib/components/Header/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+  import ThemeButton from "./ThemeButton.svelte";
+</script>
+
+<header
+  class="flex justify-between py-8 px-4 text-dark-blue-700 dark:bg-dark-blue-500 dark:text-white shadow-md"
+>
+  <h1 class="font-extrabold">Where in the world?</h1>
+  <ThemeButton />
+</header>
---
+++ rest-countries-api-main/src/lib/components/Header/ThemeButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { dark } from "../../../store";
+  import { fade } from "svelte/transition";
+  import lightIcon from "$lib/assets/light-mode.svg";
+  import darkIcon from "$lib/assets/dark-mode.svg";
+
+  const handleClick = () => {
+    dark.update((theme) => !theme);
+  };
+</script>
+
+<!-- Using if block instead of dynamically changing src, alt and mode for  -->
+<!-- transitions to work properly -->
+<div class="flex">
+  {#if $dark}
+    <button class="flex gap-2 font-semibold" on:click={handleClick} in:fade>
+      <img src={lightIcon} alt="Switch to light mode" />
+      Light mode
+    </button>
+  {:else}
+    <button class="flex gap-2 font-semibold" on:click={handleClick} in:fade>
+      <img src={darkIcon} alt="Switch to dark mode" />
+      Dark mode
+    </button>
+  {/if}
+</div>
