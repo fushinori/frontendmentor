4b1864c23d643842a5f57997fe37e72704bf53f8
Rishi <git@fushinori.me>
2023-01-12 19:13:07 +0530

    Prevent FOUC for the theme button
    Rendering based on the value of the dark store, results in a FOUC. Hence, we include both variants of the button and use the `dark:` tailwind variant to add a `display: none` accordingly. Sadly, this means we can no longer use transitions.
    
    Instead, we add a fade animation. Adding the fade animation class directly would cause the buttons to fade during their first render (page load or refresh). So we add the class to the buttons in a click event handler.

--- rest-countries-api-main/src/lib/components/Header/ThemeButton.svelte
+++ rest-countries-api-main/src/lib/components/Header/ThemeButton.svelte
@@ -1,26 +1,37 @@
 <script lang="ts">
   import { dark } from "../../../store";
-  import { fade } from "svelte/transition";
   import lightIcon from "$lib/assets/light-mode.svg";
   import darkIcon from "$lib/assets/dark-mode.svg";
 
+  let lightbutton: HTMLButtonElement;
+  let darkbutton: HTMLButtonElement;
+
   const handleClick = () => {
+    // Add animation to buttons only when clicked
+    // This prevents the animation when the buttons are first rendered
+    lightbutton.classList.add("animate-fade");
+    darkbutton.classList.add("animate-fade");
+    localStorage.setItem("theme", $dark ? "light" : "dark");
     dark.update((theme) => !theme);
+    document.documentElement.classList.toggle("dark");
   };
 </script>
 
-<!-- Using if block instead of dynamically changing src, alt and mode for  -->
-<!-- transitions to work properly -->
 <div class="flex">
-  {#if $dark}
-    <button class="flex gap-2 font-semibold" on:click={handleClick} in:fade>
-      <img src={lightIcon} alt="Switch to light mode" />
-      Light mode
-    </button>
-  {:else}
-    <button class="flex gap-2 font-semibold" on:click={handleClick} in:fade>
-      <img src={darkIcon} alt="Switch to dark mode" />
-      Dark mode
-    </button>
-  {/if}
+  <button
+    class="dark:flex hidden gap-2 font-semibold"
+    on:click={handleClick}
+    bind:this={lightbutton}
+  >
+    <img src={lightIcon} alt="Switch to light mode" />
+    Light mode
+  </button>
+  <button
+    class="flex dark:hidden gap-2 font-semibold"
+    on:click={handleClick}
+    bind:this={darkbutton}
+  >
+    <img src={darkIcon} alt="Switch to dark mode" />
+    Dark mode
+  </button>
 </div>
