2862790ffc343dae07923a32957bb49d2e2b7f95
Rishi <git@fushinori.me>
2023-01-14 22:00:22 +0530

    Fetch and alphabetically sort countries in +page.ts

--- rest-countries-api-main/src/routes/+page.ts
+++ rest-countries-api-main/src/routes/+page.ts
@@ -1,26 +1,34 @@
 import type { MinimalCountryInfo, MinimalCountryResponse } from "$lib/types";
 import type { PageLoad } from "./$types";
 
-// export const load = (async ({ fetch }) => {
-//   const res = await fetch(
-//     "https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population"
-//   );
-//   if (!res.ok) {
-//     throw new Error(`HTTP Error: ${res.status}`);
-//   }
-//   const rawCountries: MinimalCountryResponse[] = await res.json();
-//   const countries: MinimalCountryInfo[] = [];
-//   for (const country of rawCountries) {
-//     countries.push({
-//       name: country["name"]["official"],
-//       flag: country["flags"]["svg"],
-//       region: country["region"],
-//       capital: country["capital"][0],
-//       population: country["population"],
-//     });
-//   }
-//
-//   return {
-//     countries: countries,
-//   };
-// }) satisfies PageLoad;
+export const load = (async ({ fetch }) => {
+  const res = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population"
+  );
+  if (!res.ok) {
+    throw new Error(`HTTP Error: ${res.status}`);
+  }
+  const rawCountries: MinimalCountryResponse[] = await res.json();
+  const countries: MinimalCountryInfo[] = [];
+  for (const country of rawCountries) {
+    countries.push({
+      name: country["name"]["official"],
+      flag: country["flags"]["svg"],
+      region: country["region"],
+      capital: country["capital"],
+      population: country["population"],
+    });
+  }
+
+  // Alphabetically sort based on country names
+  countries.sort((a, b) => {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+    else {
+      return 1;
+    }
+  });
+
+  return {
+    countries: countries,
+  };
+}) satisfies PageLoad;
