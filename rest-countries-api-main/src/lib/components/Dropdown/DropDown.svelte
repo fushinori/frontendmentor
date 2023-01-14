2e982a6f1f20c8ec80ee0fc5bbc384336db8a035
Rishi <git@fushinori.me>
2023-01-14 21:55:56 +0530

    Style dropdown and add click handler prop

--- rest-countries-api-main/src/lib/components/Dropdown/DropDown.svelte
+++ rest-countries-api-main/src/lib/components/Dropdown/DropDown.svelte
@@ -9,10 +9,16 @@
   export let title: string;
   export let options: string[];
   export let src: string;
+  export let handleClick: (e: CustomEvent) => void;
 </script>
 
-<Menu class="text-dark-blue-100" let:open>
-  <MenuButton class="flex justify-between gap-10 bg-white rounded-sm px-8 py-4">
+<Menu
+  class="text-dark-blue-100 relative dark:text-white text-sm font-semibold max-w-max"
+  let:open
+>
+  <MenuButton
+    class="flex justify-between gap-10 bg-white dark:bg-dark-blue-500 rounded-md px-6 py-4 shadow-md shadow-gray-900"
+  >
     {title}
     <img
       {src}
@@ -21,9 +27,18 @@
       aria-hidden="true"
     />
   </MenuButton>
-  <MenuItems>
+  <MenuItems
+    class="flex flex-col absolute w-full gap-2 bg-white dark:bg-dark-blue-500 mt-2 p-4 rounded-md shadow-md shadow-gray-900"
+  >
     {#each options as option}
-      <MenuItem>{option}</MenuItem>
+      <MenuItem
+        as="button"
+        class={({ active }) =>
+          active
+            ? "text-left dark:text-cyan text-gray-700"
+            : "text-left hover:dark:text-cyan hover:text-gray-700"}
+        on:click={handleClick}>{option}</MenuItem
+      >
     {/each}
   </MenuItems>
 </Menu>
