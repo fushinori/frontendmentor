03f26dfbf8ccc39b2845e530913202309fa293f9
Rishi <git@fushinori.me>
2023-01-14 22:01:21 +0530

    Add search bar component

---
+++ rest-countries-api-main/src/lib/components/SearchBar/SearchBar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { dark, country } from "$lib/store";
+
+  export let placeholder: string;
+  export let handleInput: () => void;
+</script>
+
+<input
+  type="search"
+  class={`${
+    $dark ? "bg-light-search" : "bg-dark-search"
+  } dark:bg-dark-blue-500 bg-white font-semibold text-dark-blue-100 bg-[length:20px_20px] dark:text-white dark:placeholder:text-white bg-no-repeat bg-[5%_50%] py-4 pl-16 rounded-md text-sm w-full max-w-sm shadow-md shadow-gray-900`}
+  {placeholder}
+  bind:value={$country}
+  on:input={handleInput}
+/>
