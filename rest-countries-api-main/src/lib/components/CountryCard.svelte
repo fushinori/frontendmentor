5e52137d049642d20786b8938ee52397ab9d39a7
Rishi <git@fushinori.me>
2023-01-14 21:57:25 +0530

    Improve country card component
    Format population
    Accept more than one capital and format accordingly
    Minor stylistic changes

--- rest-countries-api-main/src/lib/components/CountryCard.svelte
+++ rest-countries-api-main/src/lib/components/CountryCard.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <article
-  class="grid grid-rows-2 bg-white dark:bg-dark-blue-500 text-dark-blue-100 dark:text-white rounded-md shadow-md shadow-[rgb(0,0,0,0.075)]"
+  class="grid grid-rows-2 bg-white dark:bg-dark-blue-500 text-dark-blue-100 dark:text-white max-w-[320px] rounded-md shadow-xl"
 >
   <img
     src={country.flag}
@@ -16,9 +16,19 @@
   <div class="flex flex-col p-6 gap-4">
     <h2 class="font-bold text-xl">{country.name}</h2>
     <div class="flex flex-col">
-      <p><span class="font-semibold">Population: </span>{country.population}</p>
+      <p>
+        <span class="font-semibold"
+          >Population:
+        </span>{country.population.toLocaleString("en-US")}
+      </p>
       <p><span class="font-semibold">Region: </span>{country.region}</p>
-      <p><span class="font-semibold">Capital: </span>{country.capital}</p>
+      <p>
+        <span class="font-semibold"
+          >{country.capital.length > 1 ? "Capitals: " : "Capital: "}</span
+        >{country.capital.length > 1
+          ? country.capital.join(", ")
+          : country.capital[0]}
+      </p>
     </div>
   </div>
 </article>
