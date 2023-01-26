33f84890b3f57225742f042e6c26d450794246b1
Rishi <git@fushinori.me>
2023-01-26 14:06:37 +0530

    Add back button component

---
+++ rest-countries-api-main/src/lib/components/Buttons/BackButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import lightArrow from "$lib/assets/back-arrow-light.svg";
+  import darkArrow from "$lib/assets/back-arrow-dark.svg";
+
+  const handleClick = () => {
+    // Remembers user's previous scroll position
+    window.history.back();
+  };
+</script>
+
+<button
+  class="bg-white dark:bg-dark-blue-500 text-dark-blue-100 dark:text-white text-xs flex max-w-[6rem] justify-center gap-2 text-center w-full py-2 shadow-[0_0_4px_2px_rgba(0,0,0,0.1)]"
+  on:click={handleClick}
+>
+  <img src={lightArrow} aria-hidden="true" class="hidden dark:block" alt="" />
+  <img src={darkArrow} aria-hidden="true" class="dark:hidden" alt="" />
+  Back
+</button>
