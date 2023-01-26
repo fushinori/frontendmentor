110d1c908fb0f225c4df7f4fe9c4ffc3bf7f93c4
Rishi <git@fushinori.me>
2023-01-26 13:19:57 +0530

    Add "button" component for border countries

---
+++ rest-countries-api-main/src/lib/components/Buttons/CountryButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let name: string;
+  export let code: string;
+</script>
+
+<a
+  class="bg-white dark:bg-dark-blue-500 text-dark-blue-100 dark:text-white text-xs text-center w-full py-1 shadow-[0_0_4px_2px_rgba(0,0,0,0.1)]"
+  href="/country/{code}"
+  target="_self"
+>
+  {name}
+</a>
