ed6df03d86ec317e51ec38b79d1cc241f2234676
Rishi <git@fushinori.me>
2023-01-26 14:05:06 +0530

    Minor stylistic changes to border country button

--- rest-countries-api-main/src/lib/components/Buttons/CountryButton.svelte
+++ rest-countries-api-main/src/lib/components/Buttons/CountryButton.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <a
-  class="bg-white dark:bg-dark-blue-500 text-dark-blue-100 dark:text-white text-xs text-center w-full py-1 shadow-[0_0_4px_2px_rgba(0,0,0,0.1)]"
+  class="bg-white dark:bg-dark-blue-500 text-dark-blue-100 dark:text-white text-xs grid place-items-center text-center w-full py-1 shadow-[0_0_4px_2px_rgba(0,0,0,0.1)]"
   href="/country/{code}"
   target="_self"
 >
