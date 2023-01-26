8f86ef914d4b00010f31c3fb6d371605b519ad55
Rishi <git@fushinori.me>
2023-01-26 13:20:39 +0530

    Add component for country properties

---
+++ rest-countries-api-main/src/lib/components/CountryProperty.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let name: string;
+  export let value: string;
+</script>
+
+<p class="text-sm">
+  <span class="font-semibold">
+    {`${name}: `}
+  </span>{value}
+</p>
