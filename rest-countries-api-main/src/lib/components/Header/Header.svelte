2d0eb9e7a32db54f15318545a618b7f51903a565
Rishi <git@fushinori.me>
2023-01-31 19:49:49 +0530

    Move shadow styles to outer div of header

--- rest-countries-api-main/src/lib/components/Header/Header.svelte
+++ rest-countries-api-main/src/lib/components/Header/Header.svelte
@@ -2,9 +2,9 @@
   import ThemeButton from "./ThemeButton.svelte";
 </script>
 
-<div class="bg-white dark:bg-dark-blue-500">
+<div class="bg-white dark:bg-dark-blue-500 shadow-md shadow-gray-900">
   <header
-    class="flex justify-between py-8 px-4 text-dark-blue-700 max-w-[1500px] mx-auto bg-white dark:bg-dark-blue-500 dark:text-white shadow-md shadow-gray-900"
+    class="flex justify-between py-8 px-4 text-dark-blue-700 max-w-[1500px] mx-auto bg-white dark:bg-dark-blue-500 dark:text-white "
   >
     <h1 class="font-extrabold"><a href="/">Where in the world?</a></h1>
     <ThemeButton />
